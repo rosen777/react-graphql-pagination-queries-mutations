3696f6ef8aba28d4641c717bb5357a58a55f2b25
Rosen Toshev <rtoscheff@gmail.com>
2023-05-09 02:29:41 +0100

    Added url input field and REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN env variable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+const TITLE = "React GraphQL GitHub";
 
-function App() {
+const axiosGitHubGraphQL = axios.create({
+  baseURL: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
+
+const App = () => {
+  const [path, setPath] = useState(
+    "the-road-to-learn-react/the-road-to-learn-react"
+  );
+
+  useEffect(() => {
+    // fetch data
+  }, []);
+
+  const handleSubmit = (e) => {
+    // fetch data
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setPath(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{TITLE}</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="url">Show open issues for https://github.com/</label>
+        <input
+          id="url"
+          type="text"
+          value={path}
+          onChange={handleChange}
+          style={{ width: "300px" }}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <hr />
     </div>
   );
-}
+};
 
 export default App;
