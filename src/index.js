93cf59f3e0dfae1a2fa41bca69ffaae6dc6c0307
Rosen Toshev <rtoscheff@gmail.com>
2023-05-16 14:57:28 +0100

    Added organization variable to the GET_REPOSITORIES_OF_ORGANIZATION querhy

--- src/index.js
+++ src/index.js
@@ -34,18 +34,46 @@ const client = new ApolloClient({
 
 console.log(`token ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`);
 
-const GET_ORGANIZATION = gql`
-  {
-    organization(login: "the-road-to-learn-react") {
+// const GET_ORGANIZATION = gql`
+//   query ($organization: String!) {
+//     organization(login: $organization) {
+//       name
+//       url
+//       repositories(first: 5) {
+//         edges {
+//           node {
+//             name
+//             url
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
+
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query ($organization: String!) {
+    organization(login: $organization) {
       name
       url
+      repositories(first: 5) {
+        edges {
+          node {
+            name
+            url
+          }
+        }
+      }
     }
   }
 `;
 
 client
   .query({
-    query: GET_ORGANIZATION,
+    query: GET_REPOSITORIES_OF_ORGANIZATION,
+    variables: {
+      organization: "the-road-to-learn-react",
+    },
   })
   .then((result) => console.log(result));
 
