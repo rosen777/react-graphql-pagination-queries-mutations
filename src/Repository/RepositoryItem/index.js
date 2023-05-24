e8643a15c2a2d2472a6a14e4e27dba06fc54580a
Rosen Toshev <rosen.toshev@postoffice.co.uk>
2023-05-25 00:46:39 +0100

    Added the addStar mutation

--- src/Repository/RepositoryItem/index.js
+++ src/Repository/RepositoryItem/index.js
@@ -1,10 +1,23 @@
 import React from "react";
-
 import { Link } from "../../Link";
+import { useMutation, gql } from "@apollo/client";
+import Button from "../../Button";
 
 import "../style.css";
 
+const STAR_REPOSITORY = gql`
+  mutation ($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
 export const RepositoryItem = ({
+  id,
   name,
   url,
   descriptionHTML,
@@ -14,35 +27,53 @@ export const RepositoryItem = ({
   watchers,
   viewerSubscription,
   viewerHasStarred,
-}) => (
-  <div>
-    <div className="RepositoryItem-title">
-      <h2>
-        <Link href={url}>{name}</Link>
-      </h2>
+}) => {
+  const [addStar, { data, loading, error }] = useMutation(STAR_REPOSITORY);
 
-      <div className="RepositoryItem-title-action">
-        {stargazers.totalCount} Stars
+  if (loading) return "Submitting...";
+  if (error) return `Submission error! ${error.message}`;
+  return (
+    <div>
+      <div className="RepositoryItem-title">
+        <h2>
+          <Link href={url}>{name}</Link>
+        </h2>
+        {!viewerHasStarred ? (
+          <Button
+            className="RepositoryItem-title-action"
+            onClick={() =>
+              addStar({
+                variables: {
+                  id,
+                },
+              })
+            }>
+            {stargazers.totalCount} Stars
+          </Button>
+        ) : (
+          <span>{/* Here comes your removeStar mutation */}</span>
+        )}
+        {/* Here comes your updateSubscription mutation */}
+      </div>
+
+      <div className="RepositoryItem-description">
+        <div
+          className="RepositoryItem-description-info"
+          dangerouslySetInnerHTML={{ __html: descriptionHTML }}
+        />
+        <div className="RepositoryItem-description-details">
+          <div>
+            {primaryLanguage && <span>Language: {primaryLanguage.name}</span>}
+          </div>
+          <div>
+            {owner && (
+              <span>
+                Owner: <a href={owner.url}>{owner.login}</a>
+              </span>
+            )}
+          </div>
+        </div>
       </div>
     </div>
-
-    <div className="RepositoryItem-description">
-      <div
-        className="RepositoryItem-description-info"
-        dangerouslySetInnerHTML={{ __html: descriptionHTML }}
-      />
-      <div className="RepositoryItem-description-details">
-        <div>
-          {primaryLanguage && <span>Language: {primaryLanguage.name}</span>}
-        </div>
-        <div>
-          {owner && (
-            <span>
-              Owner: <a href={owner.url}>{owner.login}</a>
-            </span>
-          )}
-        </div>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
